208dfd702c2a4a78d6816daf5851acc22076e3ea
Alexander Nittka <alex@nittka.de>
2020-02-09 13:40:45 +0100

    update license and copyright

--- de.itemis.tooling.terminology/src/de/itemis/tooling/terminology/TerminologyStandaloneSetup.java
+++ de.itemis.tooling.terminology/src/de/itemis/tooling/terminology/TerminologyStandaloneSetup.java
@@ -1,9 +1,11 @@
 /*******************************************************************************
- * Copyright (c) 2013 itemis AG (http://www.itemis.eu).
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (C) 2013-2020 itemis AG (http://www.itemis.eu).
+ * 
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
  ******************************************************************************/
 package de.itemis.tooling.terminology;
 
--- de.itemis.tooling.terminology/src/de/itemis/tooling/terminology/generator/SubjectEntriesSiblings.java
+++ de.itemis.tooling.terminology/src/de/itemis/tooling/terminology/generator/SubjectEntriesSiblings.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (C) 2013-2020 itemis AG (http://www.itemis.eu).
+ * 
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *    Alexander Nittka (alex@nittka.de) - initial implementation
+ ******************************************************************************/
 package de.itemis.tooling.terminology.generator;
 
 import java.util.ArrayList;
